08f6945ce3fd922a5eba674d0fa4a03f4710fd4f
Ry Biesemeyer <ry.biesemeyer@elastic.co>
2019-07-19 19:22:07 +0000

    use JRuby's `Timeout` to back `TimeoutEnforcer`

--- lib/logstash/filters/kv.rb
+++ lib/logstash/filters/kv.rb
@@ -599,82 +599,19 @@ class LogStash::Filters::KV < LogStash::Filters::Base
 
   class TimeoutEnforcer
     def initialize(logger, timeout_nanos)
-      @logger = logger
-      @running = java.util.concurrent.atomic.AtomicBoolean.new(false)
-      @timeout_nanos = timeout_nanos
-
-      # Stores running matches with their start time, this is used to cancel long running matches
-      # Is a map of Thread => start_time
-      @threads_to_start_time = java.util.concurrent.ConcurrentHashMap.new
+      @timeout_seconds = Rational(timeout_nanos, 1_000_000_000).to_f
     end
 
     def execute(&block)
-      # If the enforcer is not running, either we failed to start it or it has
-      # already been stopped; in either case, we cannot reliably enforce the timeout
-      # so we raise a RuntimeError instead.
-      fail("TimeoutEnforcer not running.") unless alive?
-
-      begin
-        thread = java.lang.Thread.currentThread()
-        @threads_to_start_time.put(thread, java.lang.System.nanoTime)
-
-        yield
-
-      rescue InterruptedRegexpError, java.lang.InterruptedException => e
-        raise TimeoutException.new
-      ensure
-        # If the block finished, but interrupt was called after, we'll want to
-        # clear the interrupted status anyway
-        @threads_to_start_time.remove(thread)
-        thread.interrupted
-      end
+      Timeout.timeout(@timeout_seconds, TimeoutException, &block)
     end
 
     def start!
-      @running.set(true)
-      @logger.debug("Starting timeout enforcer (#{@timeout_nanos}ns)")
-      @timer_thread = Thread.new do
-        while @running.get() || !@threads_to_start_time.is_empty
-          begin
-            cancel_timed_out!
-          rescue Exception => e
-            @logger.error("Error while attempting to check/cancel excessively long kv patterns",
-                          :message => e.message,
-                          :class => e.class.name,
-                          :backtrace => e.backtrace
-            )
-          end
-          sleep 0.25
-        end
-      end
+      # no-op
     end
 
     def stop!
-      @running.set(false)
-      @logger.debug("Shutting down timeout enforcer")
-      # Check for the thread mostly for a fast start/shutdown scenario
-      @timer_thread.join if @timer_thread
-    end
-
-    def alive?
-      @running.get() && @timer_thread && @timer_thread.alive?
-    end
-
-    private
-
-    def cancel_timed_out!
-      now = java.lang.System.nanoTime # save ourselves some nanotime calls
-      @threads_to_start_time.keySet.each do |thread|
-        # Use compute to lock this value
-        @threads_to_start_time.computeIfPresent(thread) do |thread, start_time|
-          if start_time < now && now - start_time > @timeout_nanos
-            thread.interrupt
-            nil # Delete the key
-          else
-            start_time # preserve the key
-          end
-        end
-      end
+      # no-op
     end
   end
 
